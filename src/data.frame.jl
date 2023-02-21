03db86d2286829fb68146ccff2016901043d6341
Dongdong Kong <kongdd@users.noreply.github.com>
2023-02-21 10:32:53 +0800

    remove match2

--- src/data.frame.jl
+++ src/data.frame.jl
@@ -1,5 +1,3 @@
-
-
 as_matrix(x::AbstractDataFrame) = Matrix(x)
 
 as_dataframe(x::AbstractDataFrame, args...) = x
@@ -21,35 +19,6 @@ datatable = dataframe
 # function list(; kw...)
 #     Dict(pairs(kw))
 # end
-
-"""
-    match2(x, y)
-
-# Examples
-```julia
-## original version
-mds = [1, 4, 3, 5]
-md = [1, 5, 6]
-
-findall(indexin(mds, md) .!= nothing)
-indexin(md, mds)
-
-## modern version
-x = [1, 2, 3, 3, 4]
-y = [0, 2, 2, 3, 4, 5, 6]
-match2(x, y)
-```
-
-# Note: match2 only find the element in `y`
-"""
-function match2(x, y)
-  # find x in y
-  ind = indexin(x, y)
-  I_x = which_notna(ind)
-  I_y = something.(ind[I_x])
-  # use `something` to suppress nothing `Union`
-  DataFrame(; value=x[I_x], I_x, I_y)
-end
 
 #! This version not work
 # function datatable(args...; kw...)
