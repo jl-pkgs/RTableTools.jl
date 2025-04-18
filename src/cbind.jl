6a47833fe941b35ac052d2f0f1af6397669e8394
Dongdong Kong <kongdd@users.noreply.github.com>
2025-04-19 00:17:27 +0800

    improve cbind

--- src/cbind.jl
+++ src/cbind.jl
@@ -1,4 +1,6 @@
 using DataFrames: hcat
+export _cbind
+export cbind, rbind
 
 # abind(args...; along=3) = cat(args..., dims=along)
 rbind(args...) = cat(args..., dims=1)
@@ -7,25 +9,19 @@ rbind(args...) = cat(args..., dims=1)
 include("cbind_helper.jl")
 # cbind = hcat # not work
 # cbind(args...) = cat(args..., dims = 2)
-function cbind end
-
-function cbind(x::AbstractDataFrame, y::AbstractDataFrame)
-  hcat(x, y; makeunique=true)
-end
-
 AbstractDataFramable = Union{AbstractDataFrame,AbstractVecOrMat}
 
-cbind(x::AbstractDataFrame, y::AbstractDataFramable) where {} =
-  hcat(as_DT(x), as_DT(y); makeunique=true)
-
-cbind(x::AbstractDataFramable, y::AbstractDataFrame) where {} =
-  hcat(as_DT(x), as_DT(y); makeunique=true)
-
-cbind(x::AbstractVecOrMat, y::AbstractVecOrMat) = 
-  hcat(x, y)
+function cbind(x::Tx, y::Ty) where {
+  Tx<:AbstractDataFramable,Ty<:AbstractDataFramable}
+  if Tx <: AbstractDataFrame || Ty <: AbstractDataFrame
+    hcat(as_DT(x), as_DT(y); makeunique=true)
+  else
+    hcat(x, y)
+  end
+end
 
 # scalar or vector
-function cbind_x(df, val, key)
+function _cbind(df::AbstractDataFrame, key::Symbol, val; head=true)
   N = nrow(df)
   n = length(val)
   if n > 1 && n < N
@@ -36,21 +32,21 @@ function cbind_x(df, val, key)
     end
   end
   df[!, key] .= val
-  df
+  df[!, Cols(key, 1:end)]
 end
 
 # 多个的排序只接受这一种
 function cbind(; deepcopy=false, kw...)
   df, keys, vals, names = get_kw_DF(; kw...)
-  
   isempty(df) && return kw
-  
+
   deepcopy && (df = Base.deepcopy(df))
   for i in eachindex(keys)
-    cbind_x(df, vals[i], keys[i])
+    _cbind(df, keys[i], vals[i])
   end
 
-  if length(unique(names)) == length(names) 
+  # 若命名无重复，则尝试修复
+  if length(unique(names)) == length(names)
     df = df[:, Cols(names)]
   end
   df
@@ -58,13 +54,8 @@ end
 
 
 function cbind(x::AbstractDataFramable, args...)
-  if length(args) == 0
-    x
-  elseif length(args) == 1
-    cbind(x, args[1])
-  else
-    cbind(cbind(x, args[1]), args[2:end]...)
-  end
+  isempty(args) && return x
+  foldl((a, b) -> cbind(a, b), args; init = x)
 end
 
 # args: 只能是 DataFrame 或者 matrix
@@ -72,29 +63,3 @@ function cbind(args...; kw...)
   r1 = cbind(args...)
   cbind(; __=as_DT(r1), kw...)
 end
-
-export cbind, rbind
-
-# # rbind = vcat
-# rbind(args...; kw...) = vcat(args...; kw...)
-# rbind(x) = x
-# rbind(x::DataFrame,
-#     y::Union{DataFrame,AbstractVecOrMat}; kw...) = begin
-#     # @assert (ncol(x) == ncol(y))
-#     x = as_DT(x)
-#     y = as_DT(y, names(x))
-#     vcat(x, y; kw...)
-# end
-
-# rbind(x::AbstractVecOrMat, y::DataFrame; kw...) = rbind(as_DT(x, names(y)), y; kw...)
-
-# function rbind(x::DataFrame, args...; kw...)
-#     x = as_DT(x)
-#     if length(args) == 0
-#         x
-#     elseif length(args) == 1
-#         rbind(x, args[1])
-#     else
-#         rbind(rbind(x, args[1]), args[2:end]...)
-#     end
-# end
--- test/test-cbind.jl
+++ test/test-cbind.jl
@@ -1,6 +1,4 @@
-using Test
-using RTableTools
-using DataFrames
+using RTableTools, DataFrames, Test
 
 df = data.table(; id=1:10, group=repeat([1, 2], 5), age=12)
 mat = as.matrix(df)
@@ -9,13 +7,15 @@ as_DF(mat)
 as_DF(mat, [:a, :b, :c])
 
 ## mainly depend on cbind(; kw...)
-@testset "cbind" begin
+@testset "cbind matrix" begin
   # mat = as_matrix(df)
   @test typeof(cbind(mat, mat, mat)) == Matrix{Int64}
   cbind(mat, mat)
   cbind(rand(4), rand(4, 4))
   cbind(rand(4), rand(4, 4), rand(4, 2))
+end
 
+@testset "cbind data.frame" begin
   @test_nowarn cbind(I=1:10, df, df)
   @test_nowarn cbind(I=1:10, df, as.matrix(df))
   @test_nowarn cbind(I=1:10, as.matrix(df), df)
@@ -26,7 +26,7 @@ end
 @testset "cbind variable order" begin
   d = DT(val=1:4)
   @test names(cbind(; x=1:4, d, z=2:5, deepcopy=true)) == ["x", "val", "z"]
-  
+    
   # matrix需要输入名字
   d = DT(val=1:4)
   cbind(; x=1, d, y=as_DT(rand(4, 4)), deepcopy=true)
